6f6d49a08968c3434767cc1ece816a1918e9414d
gilmerperez <gilmer2002@outlook.com>
2025-03-06 21:07:47 -0500

    Added Quiz component test steps

---
+++ cypress/component/Quiz.cy.jsx
@@ -0,0 +1,52 @@
+import Quiz from "../../client/src/components/Quiz";
+
+describe("Quiz Component", () => {
+  beforeEach(() => {
+    // Intercept with mock questions
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/api/questions/random",
+      },
+      {
+        fixture: "questions.json",
+        statusCode: 200,
+      }
+    ).as("getRandomQuestion");
+  });
+
+  it("should start the quiz and display the first question", () => {
+    // Loads quiz component
+    cy.mount(<Quiz />);
+    // Selects "Start Quiz" button
+    cy.get("button").contains("Start Quiz").click();
+    // Quiz card should be visible
+    cy.get(".card").should("be.visible");
+    cy.get("h2").should("not.be.empty");
+  });
+
+  it("should answer questions and complete the quiz", () => {
+    // Loads quiz component
+    cy.mount(<Quiz />);
+    // Selects "Start Quiz" button
+    cy.get("button").contains("Start Quiz").click();
+    // Answer questions
+    cy.get("button").contains("1").click();
+    // Verify quiz completion
+    cy.get(".alert-success").should("be.visible").and("contain", "Your score");
+  });
+
+  it("should restart the quiz after completion", () => {
+    // Loads quiz component
+    cy.mount(<Quiz />);
+    // Selects "Start Quiz" button
+    cy.get("button").contains("Start Quiz").click();
+    // Answer questions
+    cy.get("button").contains("1").click();
+    // Restart quiz
+    cy.get("button").contains("Take New Quiz").click();
+    // Verify the quiz is restarted
+    cy.get(".card").should("be.visible");
+    cy.get("h2").should("not.be.empty");
+  });
+});
