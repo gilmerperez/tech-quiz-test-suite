6a695648add558976efae155d4c6df82fa037d3a
gilmerperez <gilmer2002@outlook.com>
2025-03-07 20:10:45 -0500

    Refactored e2e code to be more consistent

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,32 +1,20 @@
 describe("Quiz E2E Test", () => {
-  // Before each test begins
-  beforeEach(() => {
+  it("should start the quiz and display the first question", () => {
     // Visit homepage
     cy.visit("/");
-    // Intercept with the mock question
-    // cy.intercept(
-    //   { method: "GET", url: "/api/questions/random" },
-    //   // From the pythonQuestions.json file
-    //   { fixture: "pythonQuestions.json", statusCode: 200 }
-    // ).as("getRandomQuestion");
-  });
-
-  it("should start the quiz and display the first question", () => {
-    // Selects "Start Quiz" button
+    // Select "Start Quiz" button
     cy.get("button").contains("Start Quiz").click();
     // Quiz card should be visible
-    cy.get(".card").should("be.visible");
-    // Heading should have text displayed
-    cy.get("h2").should("not.be.empty");
+    cy.get("div").should("be.visible");
+    // Heading should be visible
+    cy.get("h2").should("be.visible");
   });
 
   it("should answer questions and complete the quiz", () => {
-    // Selects "Start Quiz" button
+    cy.visit("/");
     cy.get("button").contains("Start Quiz").click();
-    // Quiz card should be visible
-    cy.get(".card").should("be.visible");
-    // Heading should have text displayed
-    cy.get("h2").should("not.be.empty");
+    cy.get("div").should("be.visible");
+    cy.get("h2").should("be.visible");
     // Answer questions
     cy.get(".card").within(() => {
       for (let i = 0; i < 10; i++) {
@@ -36,43 +24,38 @@ describe("Quiz E2E Test", () => {
   });
 
   it("should verify quiz completion", () => {
-    // Selects "Start Quiz" button
+    cy.visit("/");
     cy.get("button").contains("Start Quiz").click();
-    // Quiz card should be visible
-    cy.get(".card").should("be.visible");
-    // Heading should have text displayed
-    cy.get("h2").should("not.be.empty");
-    // Answer questions
+    cy.get("div").should("be.visible");
+    cy.get("h2").should("be.visible");
     cy.get(".card").within(() => {
       for (let i = 0; i < 10; i++) {
         cy.get("button").contains(/^\w+$/).click();
       }
     });
+    // "Quiz Completed" Heading should be visible
     cy.get("h2").contains("Quiz Completed");
-    cy.get("div").contains("Your score:");
+    // User's score should be visible and display their score
+    cy.get("div").contains("Your score: ");
   });
 
   it("should restart the quiz after completion", () => {
-    // Selects "Start Quiz" button
+    cy.visit("/");
     cy.get("button").contains("Start Quiz").click();
-    // Quiz card should be visible
-    cy.get(".card").should("be.visible");
-    // Heading should have text displayed
-    cy.get("h2").should("not.be.empty");
-    // Answer questions
+    cy.get("div").should("be.visible");
+    cy.get("h2").should("be.visible");
     cy.get(".card").within(() => {
       for (let i = 0; i < 10; i++) {
         cy.get("button").contains(/^\w+$/).click();
       }
     });
     cy.get("h2").contains("Quiz Completed");
-    cy.get("div").contains("Your score:");
+    cy.get("div").contains("Your score: ");
     // Restart quiz
     cy.get("button").contains("Take New Quiz").click();
     // Verify the quiz is restarted
-    cy.get(".card").should("be.visible");
+    cy.get("div").should("be.visible");
     // Heading should have text displayed
-    cy.get("h2").should("not.be.empty");
+    cy.get("h2").should("be.visible");
   });
 });
-
